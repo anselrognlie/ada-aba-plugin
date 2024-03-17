814704e32fd59e1b34d581d38363c1c38a374b52
Ansel Rognlie <ansel.rognlie@gmail.com>
2024-03-17 11:56:40 -0700

    fix query layout consistency

--- includes/activator.php
+++ includes/activator.php
@@ -53,57 +53,59 @@ class Activator
     $sql = [];
 
     $sql[] = "CREATE TABLE IF NOT EXISTS $learner_table_name (
-    id mediumint(9) NOT NULL AUTO_INCREMENT,
-    created_at datetime NOT NULL,
-    updated_at datetime NOT NULL,
-    deleted_at datetime,
-    first_name text NOT NULL,
-    last_name text NOT NULL,
-    email varchar(255) NOT NULL UNIQUE,
-    slug varchar(255) NOT NULL UNIQUE,
-    PRIMARY KEY  (id)
-   ) $charset_collate;
-   ";
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      created_at datetime NOT NULL,
+      updated_at datetime NOT NULL,
+      deleted_at datetime,
+      first_name text NOT NULL,
+      last_name text NOT NULL,
+      email varchar(255) NOT NULL UNIQUE,
+      slug varchar(255) NOT NULL UNIQUE,
+      PRIMARY KEY  (id)
+      ) $charset_collate;
+    ";
 
     $sql[] = "CREATE TABLE IF NOT EXISTS $course_table_name (
-    id mediumint(9) NOT NULL AUTO_INCREMENT,
-    created_at datetime NOT NULL,
-    updated_at datetime NOT NULL,
-    deleted_at datetime,
-    name text NOT NULL,
-    slug varchar(255) NOT NULL UNIQUE,
-    active tinyint(1) DEFAULT 0 NOT NULL,
-    PRIMARY KEY  (id)
-   ) $charset_collate;
-   ";
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      created_at datetime NOT NULL,
+      updated_at datetime NOT NULL,
+      deleted_at datetime,
+      name text NOT NULL,
+      slug varchar(255) NOT NULL UNIQUE,
+      active tinyint(1) DEFAULT 0 NOT NULL,
+      PRIMARY KEY  (id)
+      ) $charset_collate;
+    ";
+
+
 
     $sql[] = "CREATE TABLE IF NOT EXISTS $lesson_table_name (
-    id mediumint(9) NOT NULL AUTO_INCREMENT,
-    created_at datetime NOT NULL,
-    updated_at datetime NOT NULL,
-    deleted_at datetime,
-    name text NOT NULL,
-    slug varchar(255) NOT NULL UNIQUE,
-    PRIMARY KEY  (id)
-   ) $charset_collate;
-   ";
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      created_at datetime NOT NULL,
+      updated_at datetime NOT NULL,
+      deleted_at datetime,
+      name text NOT NULL,
+      slug varchar(255) NOT NULL UNIQUE,
+      PRIMARY KEY  (id)
+      ) $charset_collate;
+    ";
 
     $sql[] = "CREATE TABLE IF NOT EXISTS $syllabus_table_name (
-    id mediumint(9) NOT NULL AUTO_INCREMENT,
-    created_at datetime NOT NULL,
-    updated_at datetime NOT NULL,
-    deleted_at datetime,
-    course_id mediumint(9) NOT NULL,
-    lesson_id mediumint(9) NOT NULL,
-    `order` mediumint(9) NOT NULL,
-    slug varchar(255) NOT NULL UNIQUE,
-    optional tinyint(1) DEFAULT 0 NOT NULL,
-    PRIMARY KEY  (id),
-    UNIQUE KEY `course_id_lesson_id` (`course_id`,`lesson_id`),
-    FOREIGN KEY (course_id) REFERENCES $course_table_name(id),
-    FOREIGN KEY (lesson_id) REFERENCES $lesson_table_name(id)
-   ) $charset_collate;
-   ";
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      created_at datetime NOT NULL,
+      updated_at datetime NOT NULL,
+      deleted_at datetime,
+      course_id mediumint(9) NOT NULL,
+      lesson_id mediumint(9) NOT NULL,
+      `order` mediumint(9) NOT NULL,
+      slug varchar(255) NOT NULL UNIQUE,
+      optional tinyint(1) DEFAULT 0 NOT NULL,
+      PRIMARY KEY  (id),
+      UNIQUE KEY `course_id_lesson_id` (`course_id`,`lesson_id`),
+      FOREIGN KEY (course_id) REFERENCES $course_table_name(id),
+      FOREIGN KEY (lesson_id) REFERENCES $lesson_table_name(id)
+      ) $charset_collate;
+    ";
 
     $sql[] = "CREATE TABLE IF NOT EXISTS $challenge_action_table_name (
       id mediumint(9) NOT NULL AUTO_INCREMENT,
@@ -117,8 +119,8 @@ class Activator
       action_builder text NOT NULL,
       action_payload text NOT NULL,
       PRIMARY KEY  (id)
-       ) $charset_collate;
-       ";
+      ) $charset_collate;
+    ";
 
     $sql[] = "CREATE TABLE IF NOT EXISTS $enrollment_table_name (
       id mediumint(9) NOT NULL AUTO_INCREMENT,
@@ -135,8 +137,8 @@ class Activator
       UNIQUE KEY `course_id_learner_id` (`course_id`,`learner_id`),
       FOREIGN KEY (learner_id) REFERENCES $learner_table_name(id),
       FOREIGN KEY (course_id) REFERENCES $course_table_name(id)
-     ) $charset_collate;
-     ";
+      ) $charset_collate;
+    ";
 
     $sql[] = "CREATE TABLE IF NOT EXISTS $completed_lesson_table_name (
       id mediumint(9) NOT NULL AUTO_INCREMENT,
@@ -151,8 +153,8 @@ class Activator
       UNIQUE KEY `learner_id_lesson_id` (`learner_id`,`lesson_id`),
       FOREIGN KEY (learner_id) REFERENCES $learner_table_name(id),
       FOREIGN KEY (lesson_id) REFERENCES $lesson_table_name(id)
-     ) $charset_collate;
-     ";
+      ) $charset_collate;
+    ";
 
     // dbDelta doesn't officially support foreign keys
     // require_once ABSPATH . 'wp-admin/includes/upgrade.php';
