32e1a185b592d9d57481c1bbf5dbac8396a0017f
Ansel Rognlie <ansel.rognlie@gmail.com>
2024-03-21 15:50:42 -0700

    remove test json call

--- public/js/ada-aba-public.js
+++ public/js/ada-aba-public.js
@@ -2,16 +2,5 @@
   'use strict';
 
   $(function () {
-    //  console.log(window.location.pathname);
-    const doIt = async (url, nonce) => {
-      const response = await fetch(url, {
-        headers: {
-          "X-WP-Nonce": nonce,
-        },
-      });
-      const data = await response.json();
-      console.log(data);
-    }
-    doIt(`${ada_aba_vars.root}ada-aba/v1/products`, ada_aba_vars.nonce);
   });
 })(jQuery);
