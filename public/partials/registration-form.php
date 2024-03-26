005bf735534884ed23d6ef168df292f0f5563b78
Ansel Rognlie <ansel.rognlie@gmail.com>
2024-03-26 12:12:56 -0700

    add program info to registration

--- public/partials/registration-form.php
+++ public/partials/registration-form.php
@@ -16,6 +16,17 @@
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <form method="post" action="<?php echo $form_action ?>">
   <h2>Registration</h2>
+  <p>
+    Ada Build is a free, self-guided curriculum that introduces beginners to 
+    Python and coding fundamentals through tutorials and video lessons. 
+    Completing this curriculum is a great way to start your coding journey, 
+    especially for individuals seeking to strengthen their skills for the Ada 
+    application process.
+  </p>
+  <p>
+    The content covers foundational concepts such as the basics of coding 
+    languages and getting comfortable with tools.
+  </p>
   <label for="first_name">First Name</label>
   <input type="text" id="first_name" name="first_name" value="<?php echo $first_name ?>" required>
   <label for="last_name">Last Name</label>
