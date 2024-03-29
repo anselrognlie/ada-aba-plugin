e68612136861da0f3382894f6ce1badd2bc93d1b
Ansel Rognlie <ansel.rognlie@gmail.com>
2024-03-29 08:17:26 -0700

    add caching of the first sql connection

--- includes/models/wpdb-extended.php
+++ includes/models/wpdb-extended.php
@@ -4,11 +4,25 @@ namespace Ada_Aba\Includes\Models;
 
 class Wpdb_Extended
 {
+  private static $shared_mysql = null;
+  private static $shared_host = '';
+  private static $shared_name = '';
+
   private $mysqli;
 
-  public function __construct()
+  public function __construct($host = DB_HOST, $user = DB_USER, $password = DB_PASSWORD, $name = DB_NAME)
   {
-    $this->mysqli = new \mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    if (self::$shared_mysql === null) {
+      self::$shared_mysql = new \mysqli($host, $user, $password, $name);
+      self::$shared_host = $host;
+      self::$shared_name = $name;
+    }
+
+    if (self::$shared_host === $host && self::$shared_name === $name) {
+      $this->mysqli = self::$shared_mysql;
+    } else {
+      $this->mysqli = new \mysqli($host, $user, $password, $name);
+    }
   }
 
   public function get_raw_results($query)
