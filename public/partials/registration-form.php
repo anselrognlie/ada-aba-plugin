064842c74f6381105ee74a9f063f56c63a0abff4
Ansel Rognlie <ansel.rognlie@gmail.com>
2024-04-09 07:58:12 -0700

    update style and layout for live site

--- public/partials/registration-form.php
+++ public/partials/registration-form.php
@@ -14,7 +14,7 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
-<form method="post" action="<?php echo $form_action ?>">
+<form method="post" action="<?php echo $form_action ?>" class="ada-aba-registration-form">
   <h2>Registration</h2>
   <p>
     Ada Build is a free, self-guided curriculum that introduces beginners to 
@@ -27,13 +27,19 @@
     The content covers foundational concepts such as the basics of coding 
     languages and getting comfortable with tools.
   </p>
-  <label for="first_name">First Name</label>
-  <input type="text" id="first_name" name="first_name" value="<?php echo $first_name ?>" required>
-  <label for="last_name">Last Name</label>
-  <input type="text" id="last_name" name="last_name" value="<?php echo $last_name ?>" required>
-  <label for="email">Email</label>
-  <input type="email" id="email" name="email" value="<?php echo $email ?>" required>
-
+  <div class="input-group">
+    <label for="first_name">First Name</label>
+    <input type="text" id="first_name" name="first_name" value="<?php echo $first_name ?>" required>
+  </div>
+  <div class="input-group">
+    <label for="last_name">Last Name</label>
+    <input type="text" id="last_name" name="last_name" value="<?php echo $last_name ?>" required>
+  </div>
+  <div class="input-group">
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" value="<?php echo $email ?>" required>
+  </div>
+    
   <input type="hidden" name="action" value="<?php echo $action ?>">
 
   <input type="submit" value="Submit">
